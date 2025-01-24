315c865364787d699a1198f0c23a501dec1c45e4
jGYQrw gga <Kul.vova2009@yandex.ru>
2025-01-25 02:59:54 +0300

    Функция для рисования сетки, рисуем вертикальную и горизонтальную линию

--- tic_tac_toe/game_tic_tac_toe.py
+++ tic_tac_toe/game_tic_tac_toe.py
@@ -20,3 +20,14 @@ pygame.display.set_caption('Крестики-нолики')
 
 # Инициализация игрового поля
 board = [['' for _ in range(3)] for _ in range(3)]
+
+# Функция для рисования сетки, рисуем вертикальную и горизонтальную линию
+
+def draw_grid():
+   screen.fill(WHITE)
+   for i in range(1, 3):
+       pygame.draw.line(screen, BLACK, (i * CELL_SIZE, 0), (i * CELL_SIZE, WINDOW_SIZE), LINE_WIDTH)
+       pygame.draw.line(screen, BLACK, (0, i * CELL_SIZE), (WINDOW_SIZE, i * CELL_SIZE), LINE_WIDTH)
+
+
+# Функция для рисования крестиков и ноликов
