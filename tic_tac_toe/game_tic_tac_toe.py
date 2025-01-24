32a7187b0e0d3761e35fd91f137efd8f326f4be6
jGYQrw gga <Kul.vova2009@yandex.ru>
2025-01-25 02:15:46 +0300

    Инициализация игрового поля

--- tic_tac_toe/game_tic_tac_toe.py
+++ tic_tac_toe/game_tic_tac_toe.py
@@ -8,3 +8,15 @@ pygame.init()
 WINDOW_SIZE = 300
 CELL_SIZE = WINDOW_SIZE // 3
 LINE_WIDTH = 15
+
+# Цвета
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+
+# Создание окна
+screen = pygame.display.set_mode((WINDOW_SIZE, WINDOW_SIZE))
+pygame.display.set_caption('Крестики-нолики')
+
+# Инициализация игрового поля
+board = [['' for _ in range(3)] for _ in range(3)]
