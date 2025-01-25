c0bc5e3fd5154f4740578f23a13904e48077b044
jGYQrw gga <Kul.vova2009@yandex.ru>
2025-01-26 02:41:07 +0300

    Запуск игры

--- tic_tac_toe/game_tic_tac_toe.py
+++ tic_tac_toe/game_tic_tac_toe.py
@@ -58,3 +58,38 @@ def check_winner():
         return board[0][2]
 
     return None
+
+
+# Основной игровой цикл
+current_player = 'X'
+game_over = False
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        elif event.type == pygame.MOUSEBUTTONDOWN and not game_over:
+            mouseX = event.pos[0] // CELL_SIZE
+            mouseY = event.pos[1] // CELL_SIZE
+            if board[mouseY][mouseX] == '':
+                board[mouseY][mouseX] = current_player
+                if check_winner() is None:
+                    current_player = 'O' if current_player == 'X' else 'X'
+                else:
+                    game_over = True
+
+    draw_grid()
+    draw_xo()
+    pygame.display.update()
+
+    if game_over:
+        winner = check_winner()
+        font = pygame.font.Font(None, 74)
+        text = font.render(f'{winner} wins!', True, RED)
+        screen.blit(text, (WINDOW_SIZE // 2 - text.get_width() // 2, WINDOW_SIZE // 2 - text.get_height() // 2))
+        pygame.display.update()
+        pygame.time.wait(2000)
+        pygame.quit()
+        sys.exit()
+
