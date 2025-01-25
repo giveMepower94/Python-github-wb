182725e96aceb662ff38d4966d3f608abe1e6377
jGYQrw gga <Kul.vova2009@yandex.ru>
2025-01-26 01:52:36 +0300

    Функция для проверки победителя

--- tic_tac_toe/game_tic_tac_toe.py
+++ tic_tac_toe/game_tic_tac_toe.py
@@ -42,3 +42,19 @@ def draw_xo():
             elif board[row][col] == 'O':
                 pygame.draw.circle(screen, RED, (col * CELL_SIZE + CELL_SIZE // 2, row * CELL_SIZE + CELL_SIZE // 2),
                                    CELL_SIZE // 2 - 20, LINE_WIDTH)
+
+
+# Функция для проверки победителя
+def check_winner():
+    for row in range(3):
+        if board[row][0] == board[row][1] == board[row][2] != '':
+            return board[row][0]
+    for col in range(3):
+        if board[0][col] == board[1][col] == board[2][col] != '':
+            return board[0][col]
+    if board[0][0] == board[1][1] == board[2][2]:
+        return board[0][0]
+    if board[0][2] == board[1][1] == board[0][0]:
+        return board[0][2]
+
+    return None
