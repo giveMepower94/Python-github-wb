f1ad84aa4c469c818d891f2f6c4925efb8a13428
jGYQrw gga <Kul.vova2009@yandex.ru>
2025-03-22 00:39:49 +0300

    Рисуем игровое поле, используем библиотеку Colorama

--- tic_tac_toe_changed/tic_tac_toe_chahge.py
+++ tic_tac_toe_changed/tic_tac_toe_chahge.py
@@ -1,1 +1,25 @@
 # Корректируем игру крестики нолики
+# Рисуем игровое поле, используем библиотеку Colorama
+
+from colorama import Fore, Back, Style
+
+
+def draw_board(board):
+    for x in range(len(board)):
+        for y in range(len(board)):
+            if board[x][y] == " ":
+                if y < len(board) - 1:
+                    print(Back.YELLOW + board[x][y] + Style.RESET_ALL, "| ", end='')
+                else:
+                    print(Back.YELLOW + board[x][y] + Style.RESET_ALL, "| ")
+            elif board[x][y] == "X":
+                if y < len(board) - 1:
+                    print(Fore.RED + "X" + Style.RESET_ALL, "| ", end='')
+                else:
+                    print(Fore.RED + "X" + Style.RESET_ALL, "| ")
+            elif board[x][y] == "O":
+                if y < len(board) - 1:
+                    print(Fore.BLUE + "O" + Style.RESET_ALL, "| ", end='')
+                else:
+                    print(Fore.BLUE + "O" + Style.RESET_ALL, "| ")
+        print("---------")
