5198e4975c8658315574698f8209c6cd1835a479
jGYQrw gga <Kul.vova2009@yandex.ru>
2025-03-25 00:30:14 +0300

    Удаление заметки и создание функции main для запуска

--- note_project/note_project.py
+++ note_project/note_project.py
@@ -10,7 +10,7 @@ def build_note(note_text, note_name):
 
 # Напишите функцию, которая запрашивает название и текст заметки у пользователя, после чего создает заметку
 
-def crete_note():
+def create_note():
     note_name = input("Введите название заметки: ")
     note_text = input("Введите текст заметки: ")
 
@@ -50,3 +50,42 @@ def edit_note():
             print("Заметка не найдена.")
     except Exception as e:
         print(f"Произошла ошибка: {e}")
+
+# Напишите функцию, которая удаляет заметку
+
+
+def delete_note():
+    try:
+        note_name = input("Введите название заметки: ")
+        if os.path.isfile(note_name):
+            os.remove(note_name)
+            print("Заметка успешно удалена.")
+        else:
+            print("Заметка не найдена.")
+
+    except Exception as e:
+        print(f"Произошла ошибка при удалении заметки: {e}")
+
+# Напишите функцию, которая управляет работой приложения
+
+
+def main():
+    while True:
+        print('\nМеню')
+        print('1. Создать заметку')
+        print('2. Прочитать заметку')
+        print('3. Отредактировать заметку')
+        print('4. Удалить заметку')
+
+        choice = input('Выберите действие (1/2/3/4): ')
+
+        if choice == 1:
+            create_note()
+        elif choice == 2:
+            read_note()
+        elif choice == 3:
+            edit_note()
+        elif choice == 4:
+            delete_note()
+        else:
+            print("Некорректный выбор. Пожалуйста, выберите действие из меню.")
