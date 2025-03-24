96fb3914f21fb9c2bff48b704c2af5c8fc3f0003
jGYQrw gga <Kul.vova2009@yandex.ru>
2025-03-24 23:50:37 +0300

    Редактирование заметки

--- note_project/note_project.py
+++ note_project/note_project.py
@@ -1,6 +1,7 @@
 import os
 # Напишите функцию, которая создает заметку
 
+
 def build_note(note_text, note_name):
     with open(f"{note_name}.txt", "w", encoding="utf-8") as file:
         file.write(note_text)
@@ -30,3 +31,22 @@ def read_note():
     except Exception as e:
         print(f"Произошла ошибка: {e}")
 
+
+# Напишите функцию, которая редактирует заметку
+
+def edit_note():
+    try:
+        note_name = input("Введите название заметки: ")
+        if os.path.isfile(note_name):
+            print("Файл существует")
+            with open(f"{note_name}.txt", "r", encoding="utf-8") as file:
+                file.read()
+
+            new_content = input("Введите новый текст заметки: ")
+            with open(f"{note_name}.txt", "w", encoding="utf-8") as file:
+                file.write(new_content)
+                print("Заметка успешно обновлена.")
+        else:
+            print("Заметка не найдена.")
+    except Exception as e:
+        print(f"Произошла ошибка: {e}")
