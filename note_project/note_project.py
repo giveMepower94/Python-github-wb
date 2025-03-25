5314e49d2962797c8636605df219015231548bd2
jGYQrw gga <Kul.vova2009@yandex.ru>
2025-03-26 00:59:06 +0300

    Подготовка релиза приложения

--- note_project/note_project.py
+++ note_project/note_project.py
@@ -11,7 +11,6 @@ def build_note(note_text, note_name):
         print(f"Произошла ошибка: {e}")
 
 
-
 # Напишите функцию, которая запрашивает название и текст заметки у пользователя, после чего создает заметку
 
 def create_note():
@@ -76,6 +75,25 @@ def delete_note():
 # Напишите функцию, которая управляет работой приложения
 
 
+# Напишите функцию, которая выводит все заметки пользователя
+
+def display_notes():
+    try:
+        notes = [note for note in os.listdir() if note.endswith(".txt")]
+
+        notes_sorted = sorted(notes, key=lambda note: len(open(note, encoding='utf-8').read()))
+
+        for note in notes_sorted:
+            with open(note, 'r', encoding='utf-8') as file:
+                content = file.read()
+                print(f"\nСодержимое заметки '{note}':")
+                print(content)
+    except Exception as e:
+        print(f"Произошла ошибка при отображении заметок: {e}")
+
+
+# Напишите функцию, которая управляет работой приложения
+
 def main():
     while True:
         print('\nМеню')
@@ -99,20 +117,3 @@ def main():
             display_notes()
         else:
             print("Некорректный выбор. Пожалуйста, выберите действие из меню.")
-
-# Напишите функцию, которая выводит все заметки пользователя
-
-
-def display_notes():
-    try:
-        notes = [note for note in os.listdir() if note.endswith(".txt")]
-
-        notes_sorted = sorted(notes, key=lambda note: len(open(note, encoding='utf-8').read()))
-
-        for note in notes_sorted:
-            with open(note, 'r', encoding='utf-8') as file:
-                content = file.read()
-                print(f"\nСодержимое заметки '{note}':")
-                print(content)
-    except Exception as e:
-        print(f"Произошла ошибка при отображении заметок: {e}")
