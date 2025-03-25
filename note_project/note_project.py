125ac42cde39926fdb3487d51811b9d5cd8f6b6c
jGYQrw gga <Kul.vova2009@yandex.ru>
2025-03-26 00:48:30 +0300

    Создал функцию поиска упорядоченных заметок

--- note_project/note_project.py
+++ note_project/note_project.py
@@ -3,18 +3,25 @@ import os
 
 
 def build_note(note_text, note_name):
-    with open(f"{note_name}.txt", "w", encoding="utf-8") as file:
-        file.write(note_text)
-    print(f"Заметка с названием {note_name} создана!")
+    try:
+        with open(f"{note_name}.txt", "w", encoding="utf-8") as file:
+            file.write(note_text)
+            print(f"Заметка с названием {note_name} создана!")
+    except Exception as e:
+        print(f"Произошла ошибка: {e}")
+
 
 
 # Напишите функцию, которая запрашивает название и текст заметки у пользователя, после чего создает заметку
 
 def create_note():
-    note_name = input("Введите название заметки: ")
-    note_text = input("Введите текст заметки: ")
+    try:
+        note_name = input("Введите название заметки: ")
+        note_text = input("Введите текст заметки: ")
 
-    build_note(note_text, note_name)
+        build_note(note_text, note_name)
+    except Exception as e:
+        print(f"Произошла ошибка: {e}")
 
 
 # Напишите функцию, которая выводит заметку по запросу пользователя
@@ -76,8 +83,9 @@ def main():
         print('2. Прочитать заметку')
         print('3. Отредактировать заметку')
         print('4. Удалить заметку')
+        print('5. Демонстрация заметок в упорядоченном виде')
 
-        choice = input('Выберите действие (1/2/3/4): ')
+        choice = input('Выберите действие (1/2/3/4/5): ')
 
         if choice == 1:
             create_note()
@@ -87,5 +95,24 @@ def main():
             edit_note()
         elif choice == 4:
             delete_note()
+        elif choice == 5:
+            display_notes()
         else:
             print("Некорректный выбор. Пожалуйста, выберите действие из меню.")
+
+# Напишите функцию, которая выводит все заметки пользователя
+
+
+def display_notes():
+    try:
+        notes = [note for note in os.listdir() if note.endswith(".txt")]
+
+        notes_sorted = sorted(notes, key=lambda note: len(open(note, encoding='utf-8').read()))
+
+        for note in notes_sorted:
+            with open(note, 'r', encoding='utf-8') as file:
+                content = file.read()
+                print(f"\nСодержимое заметки '{note}':")
+                print(content)
+    except Exception as e:
+        print(f"Произошла ошибка при отображении заметок: {e}")
