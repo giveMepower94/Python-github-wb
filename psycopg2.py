1b5987747e2183e4a5597985ae04bae2a79ec394
jGYQrw gga <Kul.vova2009@yandex.ru>
2025-08-07 02:22:07 +0300

    Создали файл review_homework.py для проведения code_review

---
+++ psycopg2.py
@@ -0,0 +1,18 @@
+# Знакомство с работой psycopg2
+# pip install psycopg2
+import psycopg2
+
+# Создаем переменную для хранения параметров DB
+# database_config = {'host': '', 'dbname': '', 'user': '', 'password': ''}, значения не заполнены
+
+# Создаем подключение
+# conn = psycopg2.connect(**database_config)
+
+# Создаем курсор
+# cursor = conn.cursor()
+
+# Можем использовать execute
+# cursor.execute('') -- нужно добавить запрос
+
+# Вывод всех строк из запроса или одной строки
+# cursor.fetchall() or fetchone()
